c5cdfdc7d2927a6ece4eceef656dee1d570c24b7
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-06-23 14:06:18 -0700

    Create ccstartd.lua

---
+++ packages/ccstartd.lua
@@ -0,0 +1,32 @@
+--ccstartd by tomodachi94
+--mit license
+
+local function initializeDirs():
+	fs.makeDir("/bin")
+	fs.makeDir("/etc")
+	fs.makeDir("/etc")
+	fs.makeDir("/var")
+	fs.makeDir("/var/log")
+	fs.makeDir("/lib")
+	fs.makeDir("/home")
+	fs.makeDir("/share")
+	fs.makeDir("/share/help")
+end
+
+local function setPath():
+	local helpPath = help.path() .. ":/share/help"
+	local binPath = shell.path() .. "/bin"
+
+	shell.setPath(binPath)
+	help.setPath(helpPath)	
+end
+
+local function start():
+	if not fs.exists("/.ccstartd_initialized"):
+		initializeDirs()
+		f = fs.open("/.ccstartd_initialized", "w")
+		f.write("true")
+		f.close()
+	end
+	setPath()
+end
