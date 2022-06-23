812406d8fb3c3728ef892b635064323fc49c6f8c
GitHub Action <action@github.com>
2022-06-23 20:18:08 +0000

    deploy files

---
+++ packages/pig.lua
@@ -0,0 +1,10 @@
+--author::AngryCarrot789
+mon = peripheral.wrap("top")
+term.setBackgroundColor(colors.pink)
+mon.setTextScale(4)
+mon.setCursorPos(5,2)
+mon.write("OI YOU!")
+mon.setCursorPos(3,4)
+mon.write("GET OUT ME")
+mon.setCursorPos(3,5)
+mon.write("FOOKIN HEAD!")
