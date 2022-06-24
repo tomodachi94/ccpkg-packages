b467cf21d32e9f1859d48f9bf61195dc6305db39
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-06-24 12:48:48 -0700

    Add uninstall to ccpkg

--- packages/ccpkg.lua
+++ packages/ccpkg.lua
@@ -1,5 +1,5 @@
 -- ccpkg client
--- (c) 2022 tomodachi94, with help from rolcam
+-- (c) 2022 tomodachi94
 -- mit license
  
 if not http then
@@ -7,10 +7,15 @@ if not http then
     printError("If you are an administrator, set http_enable to true in ComputerCraft.cfg. If you aren't an administrator, there isn't much else you can do other than ask an admin to enable it.")
 end
  
+if not settings then
+    printError("ccpkg requires the settings API.")
+    printError("Please ask your administrator to look into installing a polyfill for it.")
+end
+ 
 local tArgs = { ... }
 local packageHub = "https://tomodachi94.github.io/ccpkg-packages/packages/"
 local destination = "/bin/"
-local destinationDocs = "/doc/"
+local destinationDocs = "/share/help/"
 local command = tArgs[1]
 local packageName = tArgs[2]
  
@@ -39,10 +44,21 @@ local function writePackage(file, path)
     end
 end
  
+local function killPackage(package)
+   if fs.exists(destination..package) then
+     fs.delete(destination..package)
+     print("Removed "..package)
+   else
+     printError("Package was never installed.")
+   end
+end
+   
 if command == "install" then
     packageFile = getPackage(packageName)
     finalDestination = destination .. packageName
     writePackage(packageFile, finalDestination)
+elseif command == "uninstall" then
+    killPackage(packageName)
 else
- print("Invalid command.")
+    printError("Invalid command.")
 end
