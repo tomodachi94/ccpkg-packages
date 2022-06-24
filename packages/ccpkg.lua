086b607321fd490911e3539c511add0d9a25cf46
GitHub Action <action@github.com>
2022-06-24 20:20:33 +0000

    deploy files

--- packages/ccpkg.lua
+++ packages/ccpkg.lua
@@ -1,64 +1,64 @@
 -- ccpkg client
 -- (c) 2022 tomodachi94
 -- mit license
- 
+
 if not http then
-    printError("ccpkg requires the HTTP API to be enabled.")
-    printError("If you are an administrator, set http_enable to true in ComputerCraft.cfg. If you aren't an administrator, there isn't much else you can do other than ask an admin to enable it.")
+	printError("ccpkg requires the HTTP API to be enabled.")
+	printError("If you are an administrator, set http_enable to true in ComputerCraft.cfg. If you aren't an administrator, there isn't much else you can do other than ask an admin to enable it.")
 end
- 
+
 if not settings then
-    printError("ccpkg requires the settings API.")
-    printError("Please ask your administrator to look into installing a polyfill for it.")
+	printError("ccpkg requires the settings API.")
+	printError("Please ask your administrator to look into installing a polyfill for it.")
 end
- 
-local tArgs = { ... }
+
+local tArgs = {...}
 local packageHub = "https://tomodachi94.github.io/ccpkg-packages/packages/"
 local destination = "/bin/"
 local destinationDocs = "/share/help/"
 local command = tArgs[1]
 local packageName = tArgs[2]
- 
+
 local function getPackage(package)
-     local url = packageHub.. package ..'.lua'
-    print("Connecting to " .. url .. "... ")
-    local response = http.get(url)
-        
-    if response then
-        print( "Success." )
-        
-        local sResponse = response.readAll()
-        response.close()
-        return sResponse
-    else
-        print( "Failed." )
-    end
+	local url = packageHub .. package .. '.lua'
+	print("Connecting to " .. url .. "... ")
+	local response = http.get(url)
+
+	if response then
+		print("Success.")
+
+		local sResponse = response.readAll()
+		response.close()
+		return sResponse
+	else
+		print("Failed.")
+	end
 end
- 
+
 local function writePackage(file, path)
-    if not fs.exists(path) then
-   file1 = fs.open(path, "w")       
-   file1.write(file)
-   file1.flush()
-   print("File written to local storage.")
-    end
+	if not fs.exists(path) then
+		file1 = fs.open(path, "w")
+		file1.write(file)
+		file1.flush()
+		print("File written to local storage.")
+	end
 end
- 
+
 local function killPackage(package)
-   if fs.exists(destination..package) then
-     fs.delete(destination..package)
-     print("Removed "..package)
-   else
-     printError("Package was never installed.")
-   end
+	if fs.exists(destination .. package) then
+		fs.delete(destination .. package)
+		print("Removed " .. package)
+	else
+		printError("Package was never installed.")
+	end
 end
-   
+
 if command == "install" then
-    packageFile = getPackage(packageName)
-    finalDestination = destination .. packageName
-    writePackage(packageFile, finalDestination)
+	packageFile = getPackage(packageName)
+	finalDestination = destination .. packageName
+	writePackage(packageFile, finalDestination)
 elseif command == "uninstall" then
-    killPackage(packageName)
+	killPackage(packageName)
 else
-    printError("Invalid command.")
+	printError("Invalid command.")
 end
--- packages/wget.lua
+++ packages/wget.lua
@@ -2,62 +2,62 @@
 --computercraft public license
 --src::https://github.com/dan200/ComputerCraft/blob/master/src/main/resources/assets/computercraft/lua/rom/programs/http/wget.lua
 local function printUsage()
-    print( "Usage:" )
-    print( "wget <url> <filename>" )
+	print("Usage:")
+	print("wget <url> <filename>")
 end
- 
-local tArgs = { ... }
+
+local tArgs = {...}
 if #tArgs < 2 then
-    printUsage()
-    return
+	printUsage()
+	return
 end
- 
+
 if not http then
-    printError( "wget requires http API" )
-    printError( "Set http_enable to true in ComputerCraft.cfg" )
-    return
+	printError("wget requires http API")
+	printError("Set http_enable to true in ComputerCraft.cfg")
+	return
 end
- 
-local function get( sUrl )
-    write( "Connecting to " .. sUrl .. "... " )
 
-    local ok, err = http.checkURL( sUrl )
-    if not ok then
-        print( "Failed." )
-        if err then
-            printError( err )
-        end
-        return nil
-    end
+local function get(sUrl)
+	write("Connecting to " .. sUrl .. "... ")
 
-    local response = http.get( sUrl , nil , true )
-    if not response then
-        print( "Failed." )
-        return nil
-    end
+	local ok, err = http.checkURL(sUrl)
+	if not ok then
+		print("Failed.")
+		if err then
+			printError(err)
+		end
+		return nil
+	end
 
-    print( "Success." )
+	local response = http.get(sUrl, nil, true)
+	if not response then
+		print("Failed.")
+		return nil
+	end
 
-    local sResponse = response.readAll()
-    response.close()
-    return sResponse
+	print("Success.")
+
+	local sResponse = response.readAll()
+	response.close()
+	return sResponse
 end
- 
+
 -- Determine file to download
 local sUrl = tArgs[1]
 local sFile = tArgs[2]
-local sPath = shell.resolve( sFile )
-if fs.exists( sPath ) then
-    print( "File already exists" )
-    return
+local sPath = shell.resolve(sFile)
+if fs.exists(sPath) then
+	print("File already exists")
+	return
 end
 
 -- Do the get
-local res = get( sUrl )
+local res = get(sUrl)
 if res then
-    local file = fs.open( sPath, "wb" )
-    file.write( res )
-    file.close()
+	local file = fs.open(sPath, "wb")
+	file.write(res)
+	file.close()
 
-    print( "Downloaded as "..sFile )
+	print("Downloaded as " .. sFile)
 end
