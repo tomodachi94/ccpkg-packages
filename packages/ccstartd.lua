ae78a5f4affc7305a5e1a7a86dab55eb69865361
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-06-24 13:20:13 -0700

    Use luafmt on packages for consistent formatting

--- packages/ccstartd.lua
+++ packages/ccstartd.lua
@@ -1,53 +1,54 @@
 --ccstartd by tomodachi94
 --mit license
- 
+
 local function initializeDirs()
-    fs.makeDir("/bin")
-    fs.makeDir("/etc")
-    fs.makeDir("/etc")
-    fs.makeDir("/var")
-    fs.makeDir("/var/log")
-    fs.makeDir("/lib")
-    fs.makeDir("/home")
-    fs.makeDir("/share")
-    fs.makeDir("/share/help")
+	fs.makeDir("/bin")
+	fs.makeDir("/etc")
+	fs.makeDir("/etc")
+	fs.makeDir("/var")
+	fs.makeDir("/var/log")
+	fs.makeDir("/lib")
+	fs.makeDir("/home")
+	fs.makeDir("/share")
+	fs.makeDir("/share/help")
 end
- 
+
 local function installStartup()
- if fs.exists("/startup") then
-   fs.move("/startup", "startup.old")
- end
- 
-  local f = fs.open("startup", "w")
-  f.write("shell.run('/bin/ccstartd')") 
-end 
- 
+	if fs.exists("/startup") then
+		fs.move("/startup", "startup.old")
+	end
+
+	local f = fs.open("startup", "w")
+	f.write("shell.run('/bin/ccstartd')")
+end
+
 local function setPath()
-    local helpPath = help.path() .. ":/share/help"
-    local binPath = shell.path() .. ":/bin"
- 
-    shell.setPath(binPath)
-    help.setPath(helpPath)  
+	local helpPath = help.path() .. ":/share/help"
+	local binPath = shell.path() .. ":/bin"
+
+	shell.setPath(binPath)
+	help.setPath(helpPath)
 end
- 
+
 local function loadLibs()
-  local libPath = "/lib"
-  -- thank you carrot
-  for k,v in pairs(fs.list(path)) do
-    os.loadAPI(v)
-  end
+	local libPath = "/lib"
+
+	-- thank you carrot
+	for k, v in pairs(fs.list(path)) do
+		os.loadAPI(v)
+	end
 end
- 
+
 local function start()
-    if not fs.exists("/.ccstartd_initialized") then
-        initializeDirs()
-  		installStartup()
-        f = fs.open("/.ccstartd_initialized", "w")
-        f.write("true")
-        f.close()
-    end
-    setPath()
- loadLibs()
+	if not fs.exists("/.ccstartd_initialized") then
+		initializeDirs()
+		installStartup()
+		f = fs.open("/.ccstartd_initialized", "w")
+		f.write("true")
+		f.close()
+	end
+	setPath()
+	loadLibs()
 end
- 
+
 start()
